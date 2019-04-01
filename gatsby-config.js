64d13668f7e8bf91b0b11a59c11af05b05d68c28
sjsakib <sjsakib.bd@gmail.com>
2019-04-01 21:30:14 +0600

    fixed config

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,7 +47,7 @@ module.exports = {
       resolve: 'gatsby-plugin-react-svg',
       options: {
         rule: {
-          include: /src\/content\/icons/,
+          include: /src\/images/,
         },
       },
     },
