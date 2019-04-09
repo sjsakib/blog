f2e4c7862c7348369cb3923cd2f4fba7589a292a
sjsakib <sjsakib.bd@gmail.com>
2019-04-09 23:50:08 +0600

    updated hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Zoom from 'react-reveal/Zoom';
-import Jump from 'react-reveal/Jump';
-import Fade from 'react-reveal/Fade';
 import { FaLongArrowAltDown } from 'react-icons/fa';
 
 import './styles/hero.scss';
 
+const inRange = (x, a, b) => a <= x && x <= b;
+
 const Hero = () => {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    setTimeout(() => {
+      setCount(count + 1);
+    }, 1000);
+  });
+  const iAm = [
+    'A problem solver',
+    'A passionate programmer',
+    'A full stack developer',
+  ];
   return (
     <section id="hero">
       <Zoom cascade ssrReveal duration={500}>
@@ -17,14 +28,27 @@ const Hero = () => {
         <h1>I'm Sakib</h1>
       </Zoom>
 
-      <Fade cascade up ssrReveal duration={500} delay={4000}>
-        <h3>A fronted developer</h3>
-      </Fade>
-      <Zoom ssrReveal delay={6000}>
+      <div className="animation-loop">
+        {iAm.map((x, i) => (
+          <Zoom
+            opposite
+            key={x}
+            duration={500}
+            when={
+              count > 3 &&
+              inRange((count + 4) % (iAm.length * 3), i * 3, i * 3 + 1)
+            }
+            collapse
+            ssrReveal
+          >
+            <h2>{x}</h2>
+          </Zoom>
+        ))}
+      </div>
+
+      <Zoom ssrReveal delay={12000}>
         <a href="/#about" className="arrow-down-link">
-          <Jump forever>
-            <FaLongArrowAltDown className="arrow-down" />
-          </Jump>
+          <FaLongArrowAltDown className="arrow-down" />
         </a>
       </Zoom>
     </section>
