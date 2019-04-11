53ee317846c5d8b37051509eb75dde73456f6df3
sjsakib <sjsakib.bd@gmail.com>
2019-04-11 20:17:24 +0600

    added ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -84,6 +84,13 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-90813960-7",
+        head: true,
+      },
+    },
     /*{
       resolve: `gatsby-plugin-typography`,
       options: {
