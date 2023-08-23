544b52acca3dd135f68a1c020ff7f4cc674f3dbf
Sakib <sjsakib.bd@gmail.com>
2023-08-23 12:00:36 +0600

    skills section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Fade from 'react-reveal/Fade';
 
 import './styles/skills.scss';
+import { Link } from 'gatsby';
 
 export default function Skills() {
   return (
@@ -10,37 +11,58 @@ export default function Skills() {
         <h1>Skills</h1>
       </Fade>
       <p>
-        <p className="skill-group">
-          <b>Languages</b> <br /> JavaScript, TypeScript, Python, Dart, SQL.
-          Familliar: PHP, Java, C, C++, Golang <br />
-        </p>
-        <p className="skill-group">
-          <b>Frontend</b> <br /> Proficient: React, Next.js, React Native, React
-          Query, GraphQL, Redux, Chrome Extensions, Flutter. Familliar: Vue.js,
-          Alpine.js <br />
-        </p>
-        <p className="skill-group">
-          <b>Backend</b> <br /> Proficient: Node.js, Express.js, Nest.js.
-          Familliar: Flask, Django <br />
-        </p>
-        <p className="skill-group">
-          <b>Database</b> <br /> MySQL, PostgresSQL, Redis. Familliar: MongoDB
-          <br />
-        </p>
-        <p className="skill-group">
-          <b>Testing/Automation</b> <br /> Puppeteer, Selenium, Jest, Cypress,
-          React Testing Library <br />
-        </p>
-        <p className="skill-group">
-          <b>Deployment</b> <br /> Docker, AWS (EC2, RDS, S3, Lambda), GitHub
-          Actions <br />
-        </p>
-        <p className="skill-group">
-          <b>Others tools and technologies</b> <br /> Babel, Webpack, ESLint,
-          SonarLint, Firebase, Sentry, Grafana, HTML, CSS, ES6+, Git, Linux,
-          Jira, Figma, Tooljet
-        </p>
+        <Fade up>
+          <p className="skill-group">
+            <b>Languages</b> <br /> JavaScript, TypeScript, Python, Dart, SQL.
+            Familliar: PHP, Java, C, C++, Golang <br />
+          </p>
+        </Fade>
+        <Fade up>
+          <p className="skill-group">
+            <b>Frontend</b> <br /> Proficient: React, Next.js, React Native,
+            React Query, GraphQL, Redux, Chrome Extensions, Flutter. Familliar:
+            Vue.js, Alpine.js <br />
+          </p>
+        </Fade>
+        <Fade up>
+          <p className="skill-group">
+            <b>Backend</b> <br /> Proficient: Node.js, Express.js, Nest.js.
+            Familliar: Flask, Django <br />
+          </p>
+        </Fade>
+        <Fade up>
+          <p className="skill-group">
+            <b>Database</b> <br /> MySQL, PostgresSQL, Redis. Familliar: MongoDB
+            <br />
+          </p>
+        </Fade>
+        <Fade up>
+          <p className="skill-group">
+            <b>Testing/Automation</b> <br /> Puppeteer, Selenium, Jest, Cypress,
+            React Testing Library <br />
+          </p>
+        </Fade>
+        <Fade up>
+          <p className="skill-group">
+            <b>Deployment</b> <br /> Docker, AWS (EC2, RDS, S3, Lambda), GitHub
+            Actions <br />
+          </p>
+        </Fade>
+        <Fade up>
+          <p className="skill-group">
+            <b>Others tools and technologies</b> <br /> Babel, Webpack, ESLint,
+            SonarLint, Firebase, Sentry, Grafana, HTML, CSS, ES6+, Git, Linux,
+            Jira, Figma, Tooljet
+          </p>
+        </Fade>
       </p>
+      <Fade up>
+        <p>
+          <Link to="/how-i-work/">
+            What I think makes me a great Software engineer
+          </Link>
+        </p>
+      </Fade>
     </section>
   );
 }
